4f951f7bfe1ce64e3b73e4fbc07d3a0dc1818b99
RogerCipher <61879622+RogerCipher@users.noreply.github.com>
2021-11-14 03:01:14 +0000

    almoust completed ukkonens algorithm

--- patterns.c
+++ patterns.c
@@ -60,12 +60,166 @@ InternalNode *createWeightedSuffixTree(int *buffer, int bufferLen)
     InternalNode *activeNode = root;
     int activeEdge = -1;
     int activeLength = 0;
-    int end = 0;
+    int *end = (int *)malloc(sizeof(int));
+    *end = 0;
+
+    InternalNode *prevCreatedNode;
+    //InternalNode *iterInternalNode;
+    RangeNode *iterRangeNode;
     for(int i = 0; i < bufferLen; i++)
     {
+        prevCreatedNode = root;
         remaining++;
-        end++;
+        *end++;
+        while(remaining)
+        {
+            if(!activeLength)
+            {
+                //it doesnt have any paths yet (only used for root)
+                if(root->pathList == NULL)
+                {
+                    root->pathList = (RangeNode *)malloc(sizeof(RangeNode));
+                    root->pathList->rangeStart = 0;
+                    root->pathList->nextInternalNode = NULL;
+                    root->pathList->rangeEnd = end;
+                    root->pathList->repeats = 0;
+                    root->pathList->sibling = NULL;
+                    remaining--;
+                    break;
+                }
+
+
+                //check if active node has a path to current buffer[i] value
+                iterRangeNode = activeNode->pathList;
+                while (iterRangeNode != NULL)
+                {
+                    if(buffer[iterRangeNode->rangeStart] == buffer[i])
+                    {
+                        //found it
+                        activeEdge = iterRangeNode->rangeStart;
+                        iterRangeNode->repeats++;
+                        activeLength++;
+                        break;
+                    }
+                    iterRangeNode = iterRangeNode->sibling;
+                }
+                //didnt find it, add this element to the range
+                iterRangeNode = activeNode->pathList;
+                while (iterRangeNode->sibling != NULL)
+                    iterRangeNode = iterRangeNode->sibling;
+
+                iterRangeNode->sibling = (RangeNode *)malloc(sizeof(RangeNode));
+                iterRangeNode->sibling->rangeStart = i;
+                iterRangeNode->sibling->nextInternalNode = NULL;
+                iterRangeNode->sibling->rangeEnd = end;
+                iterRangeNode->sibling->repeats = 0;
+                iterRangeNode->sibling->sibling = NULL;
+                remaining--;
+            }
+            else
+            {
+                //lets find our active point! first we go in the way of the right range:
+                iterRangeNode = activeNode->pathList;
+                while (iterRangeNode->rangeStart != activeEdge)
+                    iterRangeNode = iterRangeNode->sibling;
+                
+                if(iterRangeNode->rangeStart != activeLength)
+                {
+                    printf("oh no\n");
+                    exit(1);
+                }
+
+
+                //now that we are in the right track lets check if we have are going
+                //to jump an internal node on this track
+                if(activeLength > (*iterRangeNode->rangeEnd - iterRangeNode->rangeStart)+1)
+                {
+                    //the range ends here, we need to jump an internal node:
+                    activeNode = iterRangeNode->nextInternalNode;
+                    iterRangeNode = activeNode->pathList;
+                    //then we need to find the active Edge (if there is one:)
+                    while (iterRangeNode != NULL)
+                    {
+                        if(buffer[iterRangeNode->rangeStart] == buffer[i])
+                        {
+                            //there is one!
+                            activeEdge = iterRangeNode->rangeStart;
+                            iterRangeNode->repeats++;
+                            activeLength = 1;
+                            break;
+                        }
+                        iterRangeNode = iterRangeNode->sibling;
+                    }
+                    //there isnt one, what now?
+                    printf("error on the fucking ukkonens \n");
+                    exit(1);
+                    
+                }
+                else
+                {
+                    //we dont need to jump a node, lets check if the next thing in this
+                    //range is the value we want:
+                    if(buffer[iterRangeNode->rangeStart + activeLength] == buffer[i])
+                    {
+                        //it is! show stopper
+                        activeLength++;
+                        break;
+                    }
+                    //the value we want is not in the range, we need to split this range
+                    InternalNode *newInternalNode = (InternalNode *)malloc(sizeof(InternalNode));
+                    RangeNode *newRangeNode1 = (RangeNode *)malloc(sizeof(RangeNode));
+                    RangeNode *newRangeNode2 = (RangeNode *)malloc(sizeof(RangeNode));
+
+                    //new range for the new prefix
+                    newRangeNode1->nextInternalNode = NULL;
+                    newRangeNode1->sibling = newRangeNode2;
+                    newRangeNode1->rangeStart = i;
+                    newRangeNode1->rangeEnd = end;
+                    newRangeNode1->repeats = 0;
+
+                    //new range for the prefix that was there previously
+                    newRangeNode2->nextInternalNode = NULL;
+                    newRangeNode2->rangeEnd = end;
+                    newRangeNode2->rangeStart = iterRangeNode->rangeStart + activeLength;
+                    newRangeNode2->repeats = iterRangeNode->repeats -1;
+                    newRangeNode2->sibling = NULL;
+
+                    //split the old range
+                    iterRangeNode->rangeEnd = (int *)malloc(sizeof(int));
+                    *iterRangeNode->rangeEnd = iterRangeNode->rangeStart + activeLength -1;
+                    //create new ranges list
+                    newInternalNode->pathList = newRangeNode1;
+                    //conect this new internal node to the previous splitted range
+                    iterRangeNode->nextInternalNode = newInternalNode;
+
+                    //take care of the suffix link
+                    if(prevCreatedNode != root)
+                    {
+                        prevCreatedNode->suffixLink = newInternalNode;
+                        newInternalNode = prevCreatedNode;
+                    }
+                    newInternalNode->suffixLink = root;
+                    prevCreatedNode = newInternalNode;
+                    
+                    //rest of rules
+                    remaining--;
+
+                    if(activeNode == root)
+                    {
+
+                        activeLength--;
+                        activeEdge++;
+                    }
+                    else
+                    {
+                        activeNode = activeNode->suffixLink;
+                    }
+                }
+            }
+        }
     }
+
+
     return root;
 }
 
@@ -92,5 +246,9 @@ int main(int argc, char *argv[])
     printFilePatterns(patterns);
     #endif
     
+    int testBuffer[10] = {(int)'x', (int)'y', (int)'z', (int)'x', (int)'y', (int)'a', (int)'x', (int)'y', (int)'z', (int)'$'};
+    createWeightedSuffixTree(testBuffer, 10);
+
+
     return 1;
 }
